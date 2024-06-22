eff86f5e293abe05f9086e0d8e1c1a4cbdd0c2e2
zaydadnan08 <zaydadnan08@gmail.com>
2024-06-22 16:36:23 -0400

    add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ function App() {
       <header className="App-header">
         <img src={waddle} className="App-logo" alt="logo" />
         <h1 className="text-4xl text-slate-800 font-bold mt-[-50px]">
-         get waddling.
+          get waddling.
         </h1>
+        <button>
+          <a
+            className="text-2xl text-slate-800 font-bold"
+            href="https://joinwaddle.netlify.app/?name=zayd"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+            Add user 
+        </button>
+
       </header>
     </div>
   );
