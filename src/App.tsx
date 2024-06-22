bf4d516bfa8889ba250ac7274d08a9063be97331
zaydadnan08 <zaydadnan08@gmail.com>
2024-06-22 18:54:33 -0400

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import waddle from './penguin.gif';
-
 import './App.css';
 
+
 function App() {
+  
+  const handleButtonClick = () => {
+    const url = 'step-champ://?name=zaydadnan';
+    window.location.href = url;
+
+    setTimeout(() => {
+      window.location.href = 'https://yourdomain.com/fallback';  // Change this to your website's fallback URL
+    }, 1000);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,10 +21,12 @@ function App() {
         <h1 className="text-4xl text-slate-800 font-bold mt-[-50px]">
           get waddling.
         </h1>
-          <a
-            className="text-2xl text-slate-800 font-bold"
-            href="step.champ://?name=zaydadnan"
-          >Add friend here</a>
+        <button
+          className="text-2xl text-slate-800 font-bold"
+          onClick={handleButtonClick}
+        >
+          Add friend here
+        </button>
 
       </header>
     </div>
