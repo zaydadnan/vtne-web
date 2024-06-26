0a1a1e23de2f3ae3ca843e1c8c25f1dc761052f1
zaydadnan08 <zaydadnan08@gmail.com>
2024-06-26 14:40:37 -0400

    add stuff

---
+++ src/Firebase/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtcaKiYF6VoJR_i2lVRbEb5Mnzabe_cfs",
+  authDomain: "step-web-ab47d.firebaseapp.com",
+  projectId: "step-web-ab47d",
+  storageBucket: "step-web-ab47d.appspot.com",
+  messagingSenderId: "336673522798",
+  appId: "1:336673522798:web:bbcf0deb2244cb5810527f",
+  measurementId: "G-7ZHW3NZ538"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const store = getFirestore(app);
+
+export { app, store };
